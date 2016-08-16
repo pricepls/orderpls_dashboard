49be2b3fe762e860be604ba0ed8b63669a18da13
adarshraj87 <adarshraj59@gmail.com>
2016-08-17 00:29:28 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name orderplsDashboardApp
+ * @description
+ * # orderplsDashboardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('orderplsDashboardApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orderplsDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the orderplsDashboardApp
+ */
+angular.module('orderplsDashboardApp')
+  .controller('MainCtrl', function () {
+    
+  });
